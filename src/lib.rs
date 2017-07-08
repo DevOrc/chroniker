9110d65eaefd25c748a0660a4dc2391a8a7ac6f2
DevOrc <ncharlton002@gmail.com>
2017-07-08 11:18:49 -0400

    Added a function to get the UNIX time

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use std::time::Instant;
 use std::fmt;
 use std::thread;
-use std::time::Duration;
+use std::time::{SystemTime, UNIX_EPOCH, Duration};
+
 
 pub struct Timer{
     time: Instant
@@ -30,4 +31,14 @@ impl fmt::Debug for Timer {
 
 pub fn sleep(time: u64){
     thread::sleep(Duration::from_millis(time));
+}
+
+pub fn current_time_millis() -> u64{
+    let start = SystemTime::now();
+
+    let since_the_epoch = start.duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+
+    since_the_epoch.as_secs() * 1000 +
+            since_the_epoch.subsec_nanos() as u64 / 1_000_000
 }
